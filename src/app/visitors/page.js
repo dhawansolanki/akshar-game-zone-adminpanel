18841d473f132f4de766336364d60d27fc154887
Dhawan Solanki <dhawansolanki2103@gmail.com>
2024-06-24 21:05:09 +0530

    Vistors Page Updated

--- src/app/visitors/page.js
+++ src/app/visitors/page.js
@@ -1,21 +1,49 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import DashboardLayout from "../Components/Layouts/DashboardLayout";
 import DynamicTable from "../Components/Table/DynamicTable";
-import { recruiterData } from "../Components/RandomData/RandomData";
 
-const page = () => {
+const Page = () => {
+  const [visitorData, setVisitorData] = useState([]);
+
+  useEffect(() => {
+    const fetchVisitorData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.aksharenterprise.net/visitor"
+        );
+        setVisitorData(response.data);
+      } catch (error) {
+        console.error("Error fetching visitor data:", error);
+      }
+    };
+
+    fetchVisitorData();
+  }, []);
+
   const columns = [
-    { title: "Avatar", key: "imageLink" },
+    { title: "Order ID", key: "orderId" },
+    { title: "User ID", key: "userId" },
+    { title: "Phone No", key: "phoneNo" },
+    { title: "Email", key: "emailId" },
     { title: "Name", key: "name" },
-    { title: "GitHub", key: "gitHubLink" },
-    { title: "Role", key: "role" },
+    { title: "Address Line 1", key: "address.line1" },
+    { title: "Address Line 2", key: "address.line2" },
+    { title: "Address Line 3", key: "address.line3" },
+    { title: "Date of Birth", key: "dob" },
+    { title: "Anniversary Date", key: "anniversaryDate" },
+    { title: "Game", key: "game" },
+    { title: "Table", key: "table" },
+    { title: "Agreed to Terms", key: "agreeToTerms" },
   ];
+
   return (
     <DashboardLayout>
-      <h1>Hello This is RecruiterPanel</h1>
-      <DynamicTable columns={columns} data={recruiterData} />
+      <h1>Members:</h1>
+      <DynamicTable columns={columns} data={visitorData} />
     </DashboardLayout>
   );
 };
 
-export default page;
+export default Page;
