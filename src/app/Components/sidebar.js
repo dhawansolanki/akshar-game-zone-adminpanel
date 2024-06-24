98d0d79603564eea291eb91691c2f117c3e7b410
Dhawan Solanki <dhawansolanki2103@gmail.com>
2024-06-24 20:59:09 +0530

    fixed routes and removed extra files

--- src/app/Components/sidebar.js
+++ src/app/Components/sidebar.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 const Sidebar = () => {
   return (
     <div className=" text-black h-screen flex flex-col p-4">
-      <Link className="my-4" href="/AdminPanel">
+      <Link className="my-4" href="/members">
         <span className="mb-4 p-2 hover:bg-gray-700 rounded my-4">
           Members
         </span>
       </Link>
-      <Link className="my-4" href="/RecruiterPanel">
+      <Link className="my-4" href="/visitors">
         <span className="mb-4 p-2 hover:bg-gray-700 rounded my-4">
           Visitors
         </span>
